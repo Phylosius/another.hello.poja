b62d90e6ad8f4e41988cbf6f6e9ef66fc861fa5e
Phylosius <hei.phylosius@gmail.com>
2025-06-18 20:08:44 +0300

    style: format

--- src/main/java/com/hello/world/endpoint/rest/controller/index/RootController.java
+++ src/main/java/com/hello/world/endpoint/rest/controller/index/RootController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RootController {
-    
-    @GetMapping("/")
-    public String index() {
-        return "root";
-    }
+
+  @GetMapping("/")
+  public String index() {
+    return "root";
+  }
 }
